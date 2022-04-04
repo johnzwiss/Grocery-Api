5cb622584db60730a5b0a5e595543f00dd9667ab
cloud3999 <edwardlcloud@gmail.com>
2022-04-04 09:27:40 -0400

    setting up recipe model and routes

---
+++ app/models/recipe.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const ingredientSchema = new Schema ({
+    ingredient: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    qty: {
+        type: Number
+    }
+})
+
+const recipeSchema = new mongoose.Schema(
+	{
+
+        name: {
+            type: String,
+            required: true
+        },
+		ingredients: {
+            type: [ingredientSchema],
+            required: true
+        },
+		description: {
+			type: String,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Recipe', recipeSchema)
