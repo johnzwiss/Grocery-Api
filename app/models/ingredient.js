28fca0056ae2e3084b7cffd3c3b3e21361f4949d
sanchadr002 <sanchadr002@gmail.com>
2022-04-06 12:55:36 -0700

    ingredient refactor to create ingredients in database

--- app/models/ingredient.js
+++ app/models/ingredient.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose')
 
 const ingredientSchema = new mongoose.Schema ({
     ingredient: {
-        type: String
+        type: String,
+        required: true
     },
     price: {
-        type: Number
+        type: Number,
+        required: true
     },
     amount: {
         type: Number,
         min: 0    
-    }
-    
+    },
+    // owner: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User',
+    //     required: true,
+    // }
 },
 {
     timestamps: true,
